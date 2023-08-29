9fe5cd8e131309311713fa9ccf4123b53d165d5a
barneyl <barney.lee@gmail.com>
2023-08-28 17:50:18 -0700

    adjust spacing of todo rows, add 3

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <!-- Bulma class: -->
-  <div class="title has-text-centered">
-    B ToDo
-  </div>
-
   <div class="b-todo">
-    <div class="field is-grouped">
+
+    <!-- Bulma class: -->
+
+    <div class="title has-text-centered">
+      B ToDo
+    </div>
+
+    <div class="field is-grouped" mb-5>
       <p class="control is-expanded">
         <input class="input" type="text" placeholder="Add a Todo">
       </p>
@@ -14,6 +16,32 @@
           Add
         </a>
       </p>
+    </div>
+
+    <!-- v-for displays 3 items; mb-5: margin bottom -->
+    <div v-for="i in 3" class="card mb-5">
+      <div class="card-content">
+        <div class="content">
+          <div class="columns is-mobile is-vcentered">
+            <div class="column">
+              Shave head
+            </div>
+            <!-- is5 reduces size of column thus moving buttons to the right -->
+            <!-- has-text-right right justifies text -->
+            <div class="column is-5 has-text-right">
+              <button class="button is-light">
+                &check;
+              </button>
+              <!-- ml-2: margin left 2 to give left button some spacing -->
+              <button class="button is-danger ml-2">
+                &cross;
+              </button>
+            </div>
+          </div>
+          <!-- Classes are defined on Bulma.io site: -->
+
+        </div>
+      </div>
     </div>
   </div>
 </template>
